f9756ccbf94e7985e8b9df116fbbbe0f40f922cc
ScrenOne <136308085+ScrenOne@users.noreply.github.com>
2023-06-18 03:28:04 +0400

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 ﻿ return input_string.upper()
+def uppercase_string(input_string):
+    """
+    Функция, которая принимает на вход строку и возвращает ее со всеми заглавными буквами.
 
+    Parameters:
+        input_string (str): Входная строка, которую нужно преобразовать.
+
+    Returns:
+        str: Строка с заглавными буквами.
+    """
+    return input_string.upper()
